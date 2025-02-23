12b7e6084ef9ba7365327bb5672930007c482771
Litonuddinnil <mdniloyhasan544@gmail.com>
2025-02-23 15:52:04 +0600

    finally deployment completed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 const allowedOrigins = [
   "http://localhost:5173", // Local Development
@@ -77,6 +77,20 @@ async function run() {
         res.status(500).json({ message: "Error fetching tasks", error });
       }
     });
+    app.get("/tasks/:id",async(req,res)=>{
+      try {
+        const { id } = req.params;
+        const task = await tasksCollection.findById( { _id: new ObjectId(id) });
+    
+        if (!task) {
+          return res.status(404).json({ message: "Task not found" });
+        }
+    
+        res.json(task);
+      } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+      }
+    })
 
     app.post("/tasks", async (req, res) => {
       const newTask = req.body;
